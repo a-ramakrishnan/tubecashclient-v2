daf0dd62c15b145afa5a8f37f357dc55b48e7703
Ramakrishnan <ramakrishnan.a@pm.me>
2023-01-20 16:10:29 +0530

    changed devtools to false

--- src/app/store.js
+++ src/app/store.js
@@ -11,7 +11,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   //Set devTools to true during development
-  devTools: true,
+  devTools: false,
 });
 
 setupListeners(store.dispatch);
