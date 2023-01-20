5782d14bc3daedc8c5d61c3dc7967d6b5a7d5a08
Ramakrishnan <ramakrishnan.a@pm.me>
2023-01-20 16:33:13 +0530

    Fixed typo in production URL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "http://localhost:3501",
-  baseUrl: "https://https://tubecash-apiv2.onrender.com",
+  baseUrl: "https://tubecash-apiv2.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
